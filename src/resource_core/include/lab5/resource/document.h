81376585a8a330f8fc4a7c25dfcb8bc6aa035484
ivan <ivan232007@gmail.com>
2026-05-01 21:13:11 +0300

    lab5 done

---
+++ src/resource_core/include/lab5/resource/document.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <utility>
+namespace lab5::resource
+{
+class Document
+{
+  public:
+    Document(int id, std::string name, std::string text): id_(id), name_(std::move(name)), text_(std::move(text))
+    {
+    }
+    int GetId() const
+    {
+        return id_;
+    }
+    const std::string& GetName() const
+    {
+        return name_;
+    }
+    const std::string& GetText() const
+    {
+        return text_;
+    }
+
+  private:
+    int id_;
+    std::string name_;
+    std::string text_;
+};
+}
---
+++ src/resource_core/include/lab5/resource/inverted_index.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "document.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+namespace lab5::resource
+{
+class InvertedIndex
+{
+  public:
+    void AddDocument(Document&& doc);
+    void RemoveDocument(int docId);
+
+    std::vector<int> SearchWord(const std::string& word) const;
+    int GetWordCountInDocument(const std::string& word, int docId) const;
+
+  private:
+    std::unordered_map<int, Document> documents_;
+    std::unordered_map<std::string, std::unordered_map<int, int>> invertedIndex_;
+};
+}
