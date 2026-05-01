2ffb5f17b5967851dd928808b0a3330a7a666e9f
ivan <ivan232007@gmail.com>
2026-05-01 22:04:07 +0300

    lab5 done

--- src/resource_core/include/lab5/resource/document_builder.h
+++ src/resource_core/include/lab5/resource/document_builder.h
@@ -9,28 +9,28 @@ class DocumentBuilder
   public:
     DocumentBuilder& SetId(int id)
     {
-      id_ = id;
-      return *this;
+        id_ = id;
+        return *this;
     }
     DocumentBuilder& SetName(const std::string& name)
     {
-      name_ = name;
-      return *this;
+        name_ = name;
+        return *this;
     }
     DocumentBuilder& SetText(const std::string& text)
     {
-      text_ = text;
-      return *this;
+        text_ = text;
+        return *this;
     }
     Document Build()
     {
-      return Document(id_, std::move(name_), std::move(text_));
+        return Document(id_, std::move(name_), std::move(text_));
     }
 
-    private:
-      int id_ = 0;
-      std::string name_;
-      std::string text_;
+  private:
+    int id_ = 0;
+    std::string name_;
+    std::string text_;
 };
 
 } // namespace lab5::resource
